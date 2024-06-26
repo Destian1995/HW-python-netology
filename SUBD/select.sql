01e4f862902d70a24e0642d09c7c9a9aa8bf3a10
Владислав <106807250+Destian1995@users.noreply.github.com>
2024-06-26 23:47:13 +0400

    Update select.sql

--- SUBD/select.sql
+++ SUBD/select.sql
@@ -29,3 +29,8 @@ JOIN genre_performers ag ON g.id = ag.genre_id
 JOIN performers a ON ag.genre_id = a.id
 GROUP BY g.genre;
 
+SELECT COUNT(t.id) AS count_tracks
+FROM tracks t
+JOIN albums a ON t.id = a.id
+WHERE a.releaseyear BETWEEN 2019 AND 2020;
+
