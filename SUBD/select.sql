5f0565074bd0b4040722c5007ca8641d0394943a
Владислав <106807250+Destian1995@users.noreply.github.com>
2024-06-27 09:57:45 +0400

    Update select.sql

--- SUBD/select.sql
+++ SUBD/select.sql
@@ -41,13 +41,12 @@ FROM tracks t
 JOIN albums a ON t.id = a.id
 WHERE a.releaseyear BETWEEN 2019 AND 2020;
 
--- Средняя продолжительность треков по каждому альбому. Здесь вывел в секундах, в том же формате что и в таблице пока не удается, там какие-то совсем сложные запросы получатся должны с вычислениями.
-SELECT a.album, 
-       AVG((SUBSTRING(t.duration, 1, POSITION(':' IN t.duration) - 1)::INT * 60 + 
-           SUBSTRING(t.duration, POSITION(':' IN t.duration) + 1)::INT)::NUMERIC) AS average_duration
+-- Средняя продолжительность треков по каждому альбому.
+SELECT a.album, AVG(t.duration)
 FROM albums a
 JOIN tracks t ON a.id = t.id
 GROUP BY a.album;
+
 
 -- Все исполнители, которые не выпустили альбомы в 2020 году.
 SELECT DISTINCT p.performer
